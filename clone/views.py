4865900a7ad17f0947b83974e1de6c939fca27cc
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-22 15:12:31 +0300

    Create application structure with installed bootstrap3, psycopg2, python-decouple and django.

---
+++ clone/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def index(request):
+    '''
+    View function for the home page when an authenticated user is logged in
+    '''
+    pass
